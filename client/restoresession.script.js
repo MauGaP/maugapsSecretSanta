17b4be7333054f4c309b904d03e7cdcca2e51abf
MauGaP <mauro.pedano@gmail.com>
2023-12-01 23:53:06 +0100

    improved table sorting when restoring participants from session storage

--- client/restoresession.script.js
+++ client/restoresession.script.js
@@ -1,21 +1,35 @@
 function restoreFromSessionStorage() {
-  const participants = JSON.parse(sessionStorage.getItem('secretSantaAssignments'));
-  if (participants) {
+  const storedParticipants = JSON.parse(sessionStorage.getItem('secretSantaAssignments'));
+  if (storedParticipants) {
     const table = document.getElementById('friendsTable').getElementsByTagName('tbody')[0];
-    // Clear existing table rows
-    table.innerHTML = '';
+    table.innerHTML = ''; // Clear existing rows
 
-    // Iterate over participants and recreate table rows
-    participants.forEach((participant, index) => {
+    // Sort participants by their IDs
+    storedParticipants.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+
+    // Repopulate the table
+    storedParticipants.forEach(participant => {
       const row = table.insertRow();
+
+      // Hidden ID cell
       const idCell = row.insertCell(0);
+      idCell.style.display = 'none';
+      const idInput = document.createElement('input');
+      idInput.type = 'hidden';
+      idInput.value = participant.id;
+      idCell.appendChild(idInput);
+
+      // Name cell
       const nameCell = row.insertCell(1);
+      nameCell.innerHTML = `<input type="text" name="name" value="${participant.name}" onblur="saveToSessionStorage()">`;
+
+      // Email cell
       const emailCell = row.insertCell(2);
+      emailCell.innerHTML = `<input type="email" name="email" value="${participant.email}" onblur="saveToSessionStorage()">`;
+
+      // Partner cell
       const partnerCell = row.insertCell(3);
-      idCell.textContent = index + 1;
-      nameCell.innerHTML = `<input type="text" value="${participant.name}">`;
-      emailCell.innerHTML = `<input type="email" value="${participant.email}">`;
-      partnerCell.innerHTML = `<input type="text" value="${participant.partner}">`;
+      partnerCell.innerHTML = `<input type="text" name="exclude" value="${participant.partner}" onblur="saveToSessionStorage()">`;
     });
   }
 }
